774700d0bb44c0df249dbae19b85c2fe07b8136e
Nethelbert <gilblasse@gmail.com>
2019-11-09 20:28:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,9 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-# def dafdfa
-#   BRANDS << @brand if !BRANDS.include?(@brand)
-# end
+def dafdfa
+  BRANDS << @brand if !BRANDS.include?(@brand)
+end
 
 end
 
