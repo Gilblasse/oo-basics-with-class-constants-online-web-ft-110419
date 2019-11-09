9ae2d7f7371c8faae4767e49ebf02232bd69e83f
Nethelbert <gilblasse@gmail.com>
2019-11-09 20:26:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -10,6 +12,10 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+ def dafdfa
+   BRANDS << @brand if !BRANDS.include?(@brand)
+ end
 
 end
 
